a876c6c9bd628727f45e13a7232b696324967232
Salih Efe Ergur <email@salihefeergur.com.tr>
2026-01-12 20:22:55 +0000

    age calculation logic

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -7,40 +7,60 @@ import lastfm from "./assets/lastfm.svg";
 import youtube from "./assets/youtube.svg";
 import osu from "./assets/osu.svg";
 
-const ProfileCard = () => (
-    <>
-        <div className="card">
-            <div className="id">
-                <img src={pfp} alt="" />
-                <div className="info">
-                    <div className="name">Salih Efe Ergür</div>
-                    <div className="title light">Programmer</div>
-                    <div className="agelocation light">17, Turkey</div>
+const ProfileCard = () => {
+    const calculateAge = () => {
+        const birthDate = new Date('2007-09-19T00:00:00+03:00');
+        const now = new Date();
+        
+        const utcPlus3 = new Date(now.toLocaleString('en-US', { timeZone: 'Europe/Istanbul' }));
+        
+        let age = utcPlus3.getFullYear() - birthDate.getFullYear();
+        const monthDiff = utcPlus3.getMonth() - birthDate.getMonth();
+        
+        if (monthDiff < 0 || (monthDiff === 0 && utcPlus3.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        
+        return age;
+    };
+
+    const age = calculateAge();
+
+    return (
+        <>
+            <div className="card">
+                <div className="id">
+                    <img src={pfp} alt="" />
+                    <div className="info">
+                        <div className="name">Salih Efe Ergür</div>
+                        <div className="title light">Programmer</div>
+                        <div className="agelocation light">{age}, Turkey</div>
+                    </div>
+                </div>
+                <div className="bio thin">
+                    {
+                        "I'm a beginner programmer based in Turkey. I'm interested in low-level concepts and inner workings of a computer. I'm also interested in aviation and enjoy learning about aircraft systems and flight mechanics."
+                    }
+                </div>
+                <div className="socials">
+                    <SocialButton link="https://github.com/salihefee" image={github} service="GitHub" account="salihefee" />
+                    <SocialButton
+                        link="https://www.last.fm/user/pthreadgc2"
+                        image={lastfm}
+                        service="Last.fm"
+                        account="pthreadgc2"
+                    />
+                    <SocialButton
+                        link="https://www.youtube.com/channel/UCCML6jkUGPjbirZDU6_b-wA"
+                        image={youtube}
+                        service="YouTube"
+                        account="@salihefee"
+                    />
+                    <SocialButton link="https://osu.ppy.sh/u/salihefee" image={osu} service="osu!" account="salihefee" />
                 </div>
             </div>
-            <div className="bio thin">
-                {
-                    "I'm a beginner programmer based in Turkey. I'm interested in low-level concepts and inner workings of a computer. I'm also interested in aviation and enjoy learning about aircraft systems and flight mechanics."
-                }
-            </div>
-            <div className="socials">
-                <SocialButton link="https://github.com/salihefee" image={github} service="GitHub" account="salihefee" />
-                <SocialButton
-                    link="https://www.last.fm/user/pthreadgc2"
-                    image={lastfm}
-                    service="Last.fm"
-                    account="pthreadgc2"
-                />
-                <SocialButton
-                    link="https://www.youtube.com/channel/UCCML6jkUGPjbirZDU6_b-wA"
-                    image={youtube}
-                    service="YouTube"
-                    account="@salihefee"
-                />
-                <SocialButton link="https://osu.ppy.sh/u/salihefee" image={osu} service="osu!" account="salihefee" />
-            </div>
-        </div>
-    </>
-);
+        </>
+    );
+};
 
 export default ProfileCard;
